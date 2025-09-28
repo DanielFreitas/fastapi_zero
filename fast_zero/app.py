3dfb687de669a0e972d697a6a690f0b94db0eff9
DanielFreitas <daniel.freitas.jnr@gmail.com>
2025-09-28 16:33:10 -0300

    AULA 02: response html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ola', response_class=HTMLResponse)
+def ola_html():
+    html_content = """
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Olá</title>
+        </head>
+        <body>
+            <h1>olá mundo</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_deve_retornar_html_com_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'olá mundo' in response.text
